7dc3750d0a8492e96e084bb28a62c16872e90f86
Daniel Lofaro <dan@danlofaro.com>
2012-10-24 01:44:00 -0700

    added ultimedia page and tab, need to make in a good php for with headders and footers

--- multimedia.php
+++ multimedia.php
@@ -60,7 +60,7 @@
 
         <nav>
             <ul>
-                <li><a href="#main">Home</a></li>
+                <li><a href="index.php">Home</a></li>
 		<li><a href="multimedia.php">Multimedia</a></li>
 <!--                <li><a href="#services">Services</a></li>-->
 <!--                <li><a href="#portfolio">Our Works</a></li>-->
@@ -89,75 +89,6 @@
     <!-- main -->
     <section id="main">
 
-        <div class="intro-box">
-           <h1>DRC-HUBO Team</h1>
-
-           <p class="intro">Hubo is a complete full-size humanoid. Its form and function are suited for all 8 events
-    of the DARPA Robotics Challenge (DRC).  Photos on right show Hubo manually configured
-    into poses for the notional events.
-
-<br></br> DRC-Hubo will be a version with hardware retrofits and
-    algorithms for autonomy.
- 
-    7 Hubos exist at Drexel University (lead PI) and are immediately available for the DRC.</p>
-
-           <p class="intro"> Our Team:
-
-<img src="images/drcteam.png" width=500/>
-  </p>
-
-        </div>
-
-        <div class="slider-wrapper">
-
-            <div id="slider" class="nivoSlider">
-                <img src="images/slides/hubo1.png" width="383" height="198" alt="" title="Event #1 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Event #2  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   Event #3"/>
-                <img src="images/slides/hubo2.png" width="383" height="198" alt="" title="Event #4 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Event #5  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   Event #6"/>
-                <img src="images/slides/hubo3.png" width="383" height="198" alt="" title="Event #7 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Event #8  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   Virtual Hubo"/>
-		<img src="images/slides/hubo4.png" width="383" height="198" alt="" title="7 Hubos at Drexel University (lead PI)"/>
-            </div>
-            <div id="htmlcaption" class="nivo-html-caption">
-                <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>.
-            </div>
-        </div>
-
-        <div class="row no-bottom-margin">
-
-        <section class="col">
-		<h2>Hand</h2> <p> <iframe width="300" height="200" src="http://www.youtube.com/embed/9x4fnTmc6sw" frameborder="0" allowfullscreen></iframe> </p>
-	</section>
-
-	<section class="col mid">
-		<h2>2+ Hour, 2 Km Walking</h2><p><iframe width="300" height="200" src="http://www.youtube.com/embed/s_-MwnIBguE" frameborder="0" allowfullscreen></iframe></p> 
-	</section>
-
-	<section class="col">
-		<h2>Pushup</h2><p><iframe width="300" height="200" src="http://www.youtube.com/embed/nSAHf4vSrv4" frameborder="0" allowfullscreen></iframe></p> 
-	</section>
-        </div>
-
-        <a class="back-to-top" href="#main">Back to Top</a>
-
-
-<!-- Second column -->
-
-
-        <div class="row no-bottom-margin">
-
-	<section class="col">
-		<h2>Arm Speed</h2><p><iframe width="300" height="200" src="http://www.youtube.com/embed/SWCMsDax1xQ" frameborder="0" allowfullscreen></iframe></p>
-	</section>
-	<section class="col mid">
-		<h2>Ball Pickup</h2><p><iframe width="300" height="200" src="http://www.youtube.com/embed/yxqiytomejE" frameborder="0" allowfullscreen></iframe></p>
-	</section>
-
-	<section class="col">
-		<h2>Whole Body</h2><p><iframe width="300" height="200" src="http://www.youtube.com/embed/rsZN3eippW4" frameborder="0" allowfullscreen></iframe></p> 
-	</section>
-        </div>
-
-        <a class="back-to-top" href="#main">Back to Top</a>
-
 
 <!-- Third column -->
 
@@ -165,7 +96,7 @@
         <div class="row no-bottom-margin">
 
 	<section class="col">
-		<h2>Dynamic Stability</h2><p><iframe width="300" height="200" src="http://www.youtube.com/embed/w1lGeJGuaWk" frameborder="0" allowfullscreen></iframe></p>
+		<h2><center>Kickoff Simulations of Events</center></h2><p><iframe width="300" height="225" src="http://www.youtube.com/embed/KVlPowNLl4o" frameborder="0" allowfullscreen></iframe></p>
 	</section>
 	<section class="col mid">
 		<p></p>
